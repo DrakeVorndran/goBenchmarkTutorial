68516855956d0b664dcd4255f3d6f40f99ca2153
Ki Vorndran <drake.vorndran@students.makeschool.com>
2019-11-25 16:29:41 -0800

    added the test I realised I skiped

--- main_test.go
+++ main_test.go
@@ -14,6 +14,25 @@ func TestCalculate(t *testing.T) {
 	}
 }
 
+func TestTableCalculate(t *testing.T) {
+	var tests = []struct {
+		input    int
+		expected int
+	}{
+		{2, 4},
+		{-1, 1},
+		{0, 2},
+		{-5, -3},
+		{99999, 100001},
+	}
+
+	for _, test := range tests {
+		if output := Calculate(test.input); output != test.expected {
+			t.Error("Test Failed: {} inputted, {} expected, recieved: {}", test.input, test.expected, output)
+		}
+	}
+}
+
 func benchmarkCalculate(input int, b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		Calculate(input)
